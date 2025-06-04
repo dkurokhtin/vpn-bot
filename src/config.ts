d463d0bcfe3734712855b6a8b395087f58c4d28d
Dmitriy Kurokhtin <dkurokhtin@dtln.ru>
2025-06-05 00:32:07 +0300

    Исправлен env XUI_BASE_URL

--- src/config.ts
+++ src/config.ts
@@ -15,6 +15,6 @@ export const BOT_TOKEN = requireEnv('BOT_TOKEN');
 export const MONGODB_URI = requireEnv('MONGODB_URI');
 export const VPN_HOST = requireEnv('VPN_HOST');
 export const VPN_PUBLIC_KEY = requireEnv('VPN_PUBLIC_KEY');
-export const XUI_BASE_URL = process.env.XUI_BASE_URL || 'https://185.242.86.253:2053';
+export const XUI_BASE_URL = requireEnv('XUI_BASE_URL');
 export const XUI_USERNAME = requireEnv('XUI_USERNAME');
 export const XUI_PASSWORD = requireEnv('XUI_PASSWORD');
